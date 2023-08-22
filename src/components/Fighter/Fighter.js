e2bb5badfd17544970eebe2463d4a33461aab367
michael reyes <draftiercrayon4@gmail.com>
2023-08-22 08:27:00 -0400

    updated css

--- src/components/Fighter/Fighter.js
+++ src/components/Fighter/Fighter.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import "./Fighter.css";
 
 function Fighter() {
   let url = process.env.REACT_APP_API_URL;
